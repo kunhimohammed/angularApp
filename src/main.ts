5bfe575ff65c77a4f5142316a015da72172ae4bb
kunhi_mohd <km@bymacht.com>
2017-08-29 18:45:17 +0530

    Version: 1 -- Angular app

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,5 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './application/app.module';
 
-console.log({AppModule});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
